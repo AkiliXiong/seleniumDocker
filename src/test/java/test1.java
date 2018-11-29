3c2840896b8f8d64da242eee737cbd6e27e53252
Akili <sophiaxiong_82@icloud.com>
2018-11-29 16:08:33 +0800

    first checkin

---
+++ src/test/java/test1.java
@@ -0,0 +1,23 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.impl.WebDriverContainer;
+import com.codeborne.selenide.impl.WebDriverThreadLocalContainer;
+import org.testng.annotations.Test;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+
+public class test1 {
+
+    @Test
+    public void testMethod1() throws InterruptedException {
+        WebDriverContainer webDriverContainer = new WebDriverThreadLocalContainer();
+        Configuration.screenshots=false;
+        webDriverContainer.clearBrowserCache();
+        Configuration.browser="chrome";
+        Configuration.remote="http://127.0.0.1:5001/wd/hub";
+        String baseUrl = "http://www.baidu.com";
+        com.codeborne.selenide.Selenide.open(baseUrl);
+        $(byText("百度")).should(Condition.visible);
+        Thread.sleep(3000);
+    }
+}
